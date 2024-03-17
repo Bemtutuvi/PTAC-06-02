d58a34d2e2fec0d2c2eaf9c00d8bd58d68b8e32f
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-03-16 21:00:27 -0400

    lista telefonica

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
 
     return(
         <header>
-            <h1>Monster High</h1>            
+            <h1>Agenda telefônica</h1>            
         </header>
     );
 }
