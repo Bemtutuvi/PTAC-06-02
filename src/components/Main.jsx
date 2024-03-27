8dd38cbd931ef09bd2e086367d86ef9f0d6902ad
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-03-27 16:08:50 -0400

    adicionando idade e outras coisas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,22 +2,56 @@ import {useState} from "react";
 export default function Main(){
     const [nome,setNome] = useState("");
     const [telefone, setTelefone] = useState("")
+    const [idade, setIdade] = useState("")
+    const [listaContatos, setContatos]= useState([])
+
+    const registrar = (event) => {
+        event.preventDefault();
+        alert("Deu certo!");
+        setContatos([...listaContatos,{
+            nomeSalvo: nome,
+            telefoneSalvo: telefone,
+            idadeSalvo: idade
+        }]);
+    }
+    console.table(listaContatos);
     return(
         <main>
-            <form>
-                <label For="nome">Nome:</label>
+            <form onSubmit={registrar}>
+                <label htmlFor="nome">Nome</label>
                 <input
                      type="text"
-                     name=""
-                        id=""
+                     name="nome-contato"
+                     id="nome"
                        onChange={(event)=> setNome(event.target.value)}/>
                      {nome}
-            </form>
-            <form>
-                <label For="telefone">Telefone</label>
-                <input type="tel" name="" id="" onChange={(event)=> setTelefone(event.target,value)}/>
+                     <br></br>
+                     <br></br>
+           
+                <label htmlFor="telefone">Telefone</label>
+                 {/*onChange= Atributo que quadno altera o input dispara uma funcao--!>*/}
+                {/*} setTelefone /salva a informacao/ (informacao)*/}
+                <input 
+                    type="tel" 
+                    name="telefone-contato" 
+                    id="telefone" 
+                    onChange={(event)=> setTelefone(event.target.value)}/>
                 {telefone}
+                
+                <br></br>
+                <br></br>
+                
+                <laber htmlform="idade">Idade</laber>
+                <input
+                    type="age"
+                    name="idade-contato"
+                    id="idade"
+                    onChange={(event)=> setIdade(event.target.value)}/>
+                
+                <br></br>
                 <button type="submit">Enviar</button>
+
+
             </form>
         </main>
 
